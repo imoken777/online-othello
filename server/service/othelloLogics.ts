9884dbf50749bc82e80b3671aa87939207984514
imoken777 <s1f102301640@iniad.org>
2024-03-05 18:23:47 +0900

    オセロのメインロジックとその保存

---
+++ server/service/othelloLogics.ts
@@ -0,0 +1,105 @@
+// 盤面上の指定位置が有効か判断する関数
+const isOnBoard = (x: number, y: number, board: number[][]): boolean => {
+  return x >= 0 && x < board[0].length && y >= 0 && y < board.length;
+};
+
+// 特定の方向に石をひっくり返せるかどうかを判定する関数
+export const canFlipInDirection = (
+  startX: number,
+  startY: number,
+  dx: number,
+  dy: number,
+  board: number[][],
+  turnColor: number
+): boolean => {
+  let x = startX + dx;
+  let y = startY + dy;
+  let canFlip = false;
+
+  while (isOnBoard(x, y, board)) {
+    if (board[y][x] === 0) {
+      return false;
+    } else if (board[y][x] !== turnColor) {
+      canFlip = true;
+      x += dx;
+      y += dy;
+    } else {
+      return canFlip;
+    }
+  }
+  return false;
+};
+
+// 盤面上の指定位置に石を置けるかどうかを判定する関数
+export const canPlaceStone = (
+  x: number,
+  y: number,
+  board: number[][],
+  turnColor: number
+): boolean => {
+  if (board[y][x] !== 0) {
+    return false;
+  }
+
+  const directions = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+  ];
+
+  return directions.some(([dx, dy]) => canFlipInDirection(x, y, dx, dy, board, turnColor));
+};
+
+// 盤面上の指定位置が相手の石かどうかを判断する関数
+const isOpponentStone = (x: number, y: number, board: number[][], turnColor: number): boolean => {
+  return board[y][x] !== 0 && board[y][x] !== turnColor;
+};
+
+// 盤面上の特定の方向でひっくり返せる石の位置を見つける関数
+const findStonesToFlip = (
+  startX: number,
+  startY: number,
+  dx: number,
+  dy: number,
+  board: number[][],
+  turnColor: number
+): number[][] => {
+  let x = startX + dx;
+  let y = startY + dy;
+  const path = [];
+
+  // 盤面の範囲内であり、相手の石である間ループ
+  while (isOnBoard(x, y, board) && isOpponentStone(x, y, board, turnColor)) {
+    path.push([x, y]);
+    x += dx;
+    y += dy;
+  }
+
+  // ループの終了条件が自分の石であれば、pathを返す
+  if (isOnBoard(x, y, board) && board[y][x] === turnColor) {
+    return path;
+  }
+
+  return [];
+};
+
+// 盤面上の特定の方向にある石をひっくり返す関数
+export const flipStonesInDirection = (
+  startX: number,
+  startY: number,
+  dx: number,
+  dy: number,
+  board: number[][],
+  turnColor: number
+): number[][] => {
+  const stonesToFlip = findStonesToFlip(startX, startY, dx, dy, board, turnColor);
+  for (const [x, y] of stonesToFlip) {
+    board[y][x] = turnColor;
+  }
+  return board;
+};
--- server/useCase/boardUseCase.ts
+++ server/useCase/boardUseCase.ts
@@ -1,31 +1,44 @@
 import type { RoomId, UserId } from '$/commonTypesWithClient/ids';
 import type { RoomModel } from '$/commonTypesWithClient/models';
 import { roomRepository } from '$/repository/roomRepository';
+import { canFlipInDirection, canPlaceStone, flipStonesInDirection } from '$/service/othelloLogics';
 import { userColorUseCase } from './userColorUseCase';
 
-export type BoardArray = number[][];
-
-const board: BoardArray = [
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 1, 2, 0, 0, 0],
-  [0, 0, 0, 2, 1, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0],
+const directions = [
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, -1],
+  [0, 1],
+  [1, -1],
+  [1, 0],
+  [1, 1],
 ];
-
 export const boardUseCase = {
   clickBoard: async (x: number, y: number, userId: UserId, roomId: RoomId): Promise<RoomModel> => {
     const room = await roomRepository.findById(roomId);
     if (!room) {
       throw new Error('no room');
     }
+    const turnColor = userColorUseCase.getUserColor(userId, room);
+    if (room.currentTurn !== turnColor) return room;
+
     const newBoard: number[][] = JSON.parse(JSON.stringify(room.board));
 
-    newBoard[y][x] = userColorUseCase.getUserColor(userId, room);
-    const newRoom: RoomModel = { ...room, board: newBoard };
+    const isCanPlaceStone = canPlaceStone(x, y, newBoard, turnColor);
+    if (!isCanPlaceStone) return room;
+
+    newBoard[y][x] = turnColor;
+
+    room.currentTurn = 3 - turnColor; // ターンを交代
+
+    directions.forEach(([dx, dy]) => {
+      if (canFlipInDirection(x, y, dx, dy, newBoard, turnColor)) {
+        flipStonesInDirection(x, y, dx, dy, newBoard, turnColor);
+      }
+    });
+
+    const newRoom: RoomModel = { ...room, board: newBoard, currentTurn: room.currentTurn };
     await roomRepository.updateBoard(newRoom);
     return newRoom;
   },
