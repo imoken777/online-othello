5a7176b7dc4e8e976abb33f3f62a2827923d794e
imoken777 <s1f102301640@iniad.org>
2024-03-06 18:51:54 +0900

    Revert "再接続のためのapi"
    This reverts commit 96dc86a6013532c212456891286baf52bed2b4e5.

--- server/api/rooms/connect/controller.ts
+++
@@ -1,6 +0,0 @@
-import { roomRepository } from '$/repository/roomRepository';
-import { defineController } from './$relay';
-
-export default defineController(() => ({
-  get: async ({ user }) => ({ status: 200, body: await roomRepository.findActiveRooms(user.id) }),
-}));
