d2c20c1180dde6d1dafc57bca5a44a16b482ee75
imoken777 <s1f102301640@iniad.org>
2024-03-27 23:46:18 +0900

    apiClientのベースURLを環境変数から取得するように変更

--- client/src/utils/apiClient.ts
+++ client/src/utils/apiClient.ts
@@ -2,4 +2,6 @@ import aspida from '@aspida/axios';
 import api from 'api/$api';
 import axios from 'axios';
 
-export const apiClient = api(aspida(axios.create({ withCredentials: true })));
+const baseURL = process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:31577/api';
+
+export const apiClient = api(aspida(axios.create({ withCredentials: true, baseURL })));
